fec73c9a90e0e27cbdac7e540c20abe2bfcd6287
Karl Steven <54846911+karlorjalo@users.noreply.github.com>
2020-11-03 16:36:45 +0800

    Update XBEE_TX.ino

--- src/Xbee_transmitter/XBEE_TX.ino
+++ src/Xbee_transmitter/XBEE_TX.ino
@@ -1,23 +1,25 @@
 /**
    Copyright (c) 2009 Andrew Rapp. All rights reserved.
-
    This file is part of XBee-Arduino.
-
    XBee-Arduino is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-
    XBee-Arduino is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
    You should have received a copy of the GNU General Public License
    along with XBee-Arduino.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <XBee.h>
+#include <Wire.h> // Used to establied serial communication on the I2C bus
+#include <SparkFunTMP102.h> // Used to send and recieve specific information from our sensor
+
+
+const int ALERT_PIN = A3;
+TMP102 sensor0;
 
 /*
   This example is for Series 2 XBee
@@ -27,7 +29,7 @@
 // create the XBee object
 XBee xbee = XBee();
 
-uint8_t payload[] = { 0, 0, 0 };
+uint8_t payload[] = { 0, 0, 0, 0, 0, 0, 0 };
 
 // SH + SL Address of receiving XBee
 XBeeAddress64 addr64 = XBeeAddress64(0x13A200, 0x4194502C);
@@ -60,23 +62,45 @@ union floatByte_t {
 };
 
 void setup() {
+  Wire.begin(); //Join I2C Bus
+  pinMode(ALERT_PIN,INPUT); 
   pinMode(statusLed, OUTPUT);
   pinMode(errorLed, OUTPUT);
-
   Serial.begin(9600);
   Serial1.begin(9600);
   xbee.setSerial(Serial1);
+
+  if(!sensor0.begin())
+  {
+    Serial.println("Cannot connect to TMP102.");
+    Serial.println("Is the board connected? Is the device ID correct?");
+    while(1);
+  }
 
 }
 
 void loop() {
   // pack "ilovetocode" into the payload
+  sensor0.wakeup();
+  float temperature;
+  boolean alertPinState, alertRegisterState; 
   int i;
+  int temp;
   int HIH4030_Value = analogRead(humiditySensor);
   payload[0] = HIH4030_Value / 100 % 10; 
   payload[1] = HIH4030_Value / 10 % 10;
   payload[2] = HIH4030_Value % 10;
   Serial.println(HIH4030_Value);
+  temperature = sensor0.readTempC();
+  Serial.println(temperature);
+  temp = temperature * 100;
+  payload[3] = temp / 1000 % 10; 
+  payload[4] = temp / 100 % 10;
+  payload[5] = temp / 10 % 10; 
+  payload[6] = temp % 10; 
+   
+  
+  
   
   
  
