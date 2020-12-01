fdb57c86b6bb5c204eaa927372b2b92807685bf7
Arash <47273556+arashnrim@users.noreply.github.com>
2020-12-01 11:47:30 +0800

    🎨 Run AutoPEP8 linter
    This commit runs the AutoPEP8 linter to ensure a standardised code
    structure.
    
    Though if you ask me, it looks a little unreadable... :thinking:

--- src/parser.py
+++ src/parser.py
@@ -1,10 +1,10 @@
 import io
-import os
 from pathlib import Path
 
 while True:
     input_file = input("Enter the full path of the recording file: ")
-    output_directory = input("Enter the (existing) directory to save the outputs: ")
+    output_directory = input(
+        "Enter the (existing) directory to save the outputs: ")
 
     with io.open(input_file, encoding="utf-8") as file:
         lines = file.readlines()
@@ -17,21 +17,26 @@ while True:
             continue
         except FileExistsError:
             print("Files found, proceed?", end=" ")
-            proceed = bool(input("Leave blank to end program, type anything to continue: "))
-            if not(proceed): break
+            proceed = bool(
+                input("Leave blank to end program, type anything to continue: "))
+            if not proceed:
+                break
 
         # Saves humidity values into a separate file
-        with io.open("{}/recording_output/humidity.txt".format(output_directory), "w", encoding="utf-8") as humidity_file:
+        with io.open("{}/recording_output/humidity.txt".format(output_directory), "w",
+                     encoding="utf-8") as humidity_file:
             for line in lines:
                 try:
                     if line.split()[0] == "Humidity:":
-                        humidity_file.write("{}\n".format(line.split()[1][:-1]))
+                        humidity_file.write(
+                            "{}\n".format(line.split()[1][:-1]))
                 except IndexError:
                     pass
             print("Humidity data extracted.")
 
         # Saves temperature values into a separate file
-        with io.open("{}/recording_output/temperature.txt".format(output_directory), "w", encoding="utf-8") as temp_file:
+        with io.open("{}/recording_output/temperature.txt".format(output_directory), "w",
+                     encoding="utf-8") as temp_file:
             for line in lines:
                 try:
                     if line.split()[0] == "Temperature:":
@@ -41,11 +46,13 @@ while True:
             print("Temperature data extracted.")
 
         # Saves pressure values into a separate file
-        with io.open("{}/recording_output/pressure.txt".format(output_directory), "w", encoding="utf-8") as pressure_file:
+        with io.open("{}/recording_output/pressure.txt".format(output_directory), "w",
+                     encoding="utf-8") as pressure_file:
             for line in lines:
                 try:
                     if line.split()[0] == "Pressure:":
-                        pressure_file.write("{}\n".format(line.split()[1][:-3]))
+                        pressure_file.write(
+                            "{}\n".format(line.split()[1][:-3]))
                 except IndexError:
                     pass
             print("Pressure data extracted.")
@@ -53,31 +60,38 @@ while True:
         axes = ["x-axis", "y-axis", "z-axis"]
         # Saves 9DOF values into a separate file
         for axis in axes:
-            with io.open("{}/recording_output/{}_accel.txt".format(output_directory, axis), "w", encoding="utf-8") as accel_file:
+            with io.open("{}/recording_output/{}_accel.txt".format(output_directory, axis), "w",
+                         encoding="utf-8") as accel_file:
                 for line in lines:
                     try:
                         if line.split()[0] == axis and line.split()[1] == "acceleration:":
-                            accel_file.write("{}\n".format(line.split()[2][:-5]))
+                            accel_file.write(
+                                "{}\n".format(line.split()[2][:-5]))
                     except IndexError:
                         pass
                 print("{} acceleration data extracted.".format(axis.capitalize()))
 
-            with io.open("{}/recording_output/{}_magnet.txt".format(output_directory, axis), "w", encoding="utf-8") as magnet_file:
+            with io.open("{}/recording_output/{}_magnet.txt".format(output_directory, axis), "w",
+                         encoding="utf-8") as magnet_file:
                 for line in lines:
                     try:
                         if line.split()[0] == axis and line.split()[1] == "magnetometer:":
-                            magnet_file.write("{}\n".format(line.split()[2][:-2]))
+                            magnet_file.write(
+                                "{}\n".format(line.split()[2][:-2]))
                     except IndexError:
                         pass
                 print("{} magnetometer data extracted.".format(axis.capitalize()))
 
-            with io.open("{}/recording_output/{}_gyro.txt".format(output_directory, axis), "w", encoding="utf-8") as gyro_file:
+            with io.open("{}/recording_output/{}_gyro.txt".format(output_directory, axis), "w",
+                         encoding="utf-8") as gyro_file:
                 for line in lines:
                     try:
                         if line.split()[0] == axis and line.split()[1] == "gyroscope:":
-                            gyro_file.write("{}\n".format(line.split()[2][:-5]))
+                            gyro_file.write(
+                                "{}\n".format(line.split()[2][:-5]))
                         elif len(line.split()) == 1:
-                            gyro_file.write("{}\n".format(line.split()[0][:-5]))
+                            gyro_file.write(
+                                "{}\n".format(line.split()[0][:-5]))
                     except IndexError:
                         pass
                 print("{} gyroscope data extracted.".format(axis.capitalize()))
